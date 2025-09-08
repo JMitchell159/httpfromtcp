ebdd27a3421230d9567b63b8ad691cd153f7de96
JMichell159 <joemitchell159@gmail.com>
2025-09-08 07:10:02 -0500

    main now reads a file 8 bytes at a time

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"io"
+	"os"
+)
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	file, err := os.Open("messages.txt")
+	if err != nil {
+		fmt.Printf("error when reading messages.txt: %v", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	buff := make([]byte, 8)
+	for {
+		if _, err = file.Read(buff); errors.Is(err, io.EOF) {
+			os.Exit(0)
+		}
+		fmt.Printf("read: %s\n", buff)
+		clear(buff)
+	}
 }
