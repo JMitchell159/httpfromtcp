bf8c4e57ad0ca6fef4732b6b130e83cfe491ee44
JMichell159 <joemitchell159@gmail.com>
2025-09-10 06:48:51 -0500

    created getLinesChannel, main accepts input from tcp connection
    getLinesChannel function serves to abstract the process of reading data 8 bytes at a time, while sending one line at a time to the caller of the function

--- main.go
+++ main.go
@@ -4,39 +4,68 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"net"
 	"os"
 	"strings"
 )
 
-func main() {
-	file, err := os.Open("messages.txt")
-	if err != nil {
-		fmt.Printf("error when opening messages.txt: %v", err)
-		os.Exit(1)
-	}
-	defer file.Close()
-
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	lineChan := make(chan string)
 	current := ""
-	for {
-		buff := make([]byte, 8)
-		n, err := file.Read(buff)
-		str := string(buff[:n])
-		parts := strings.Split(str, "\n")
-		for _, l := range parts[:len(parts)-1] {
-			fmt.Printf("read: %s%s\n", current, l)
-			current = ""
-		}
-		current += parts[len(parts)-1]
-		if err != nil {
-			if current != "" {
-				fmt.Printf("read: %s\n", current)
+	go func() {
+		for {
+			buff := make([]byte, 8)
+			n, err := f.Read(buff)
+			str := string(buff[:n])
+			parts := strings.Split(str, "\n")
+			for _, l := range parts[:len(parts)-1] {
+				lineChan <- current + l
 				current = ""
 			}
-			if errors.Is(err, io.EOF) {
+			current += parts[len(parts)-1]
+			if err != nil {
+				if current != "" {
+					lineChan <- current
+					current = ""
+				}
+				if errors.Is(err, io.EOF) {
+					close(lineChan)
+					f.Close()
+					break
+				}
+				fmt.Printf("error: %s\n", err.Error())
+				close(lineChan)
+				f.Close()
 				break
 			}
-			fmt.Printf("error: %s\n", err.Error())
-			break
 		}
+	}()
+
+	return lineChan
+}
+
+func main() {
+	tcpListener, err := net.Listen("tcp", ":42069")
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		os.Exit(1)
+	}
+	defer tcpListener.Close()
+
+	for {
+		connect, err := tcpListener.Accept()
+		if err != nil {
+			fmt.Printf("%v\n", err)
+			os.Exit(1)
+		}
+
+		fmt.Println("Connection has been accepted")
+
+		lines := getLinesChannel(connect)
+		for line := range lines {
+			fmt.Printf("%s\n", line)
+		}
+
+		fmt.Println("Connection has been closed")
 	}
 }
